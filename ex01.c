d3fb1ac39b0436cabd68c350bd2b0dcf181b6493
ertoromartinez10 <156683516+ertoromartinez10@users.noreply.github.com>
2024-01-15 15:19:38 +0100

    Add files via upload

---
+++ ex01.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+    int y = 4.3;
+    char l = 4;
+    float x = 4.23;
+    short w = 4;
+    long o = 79;
+    float e = 3.54;
+
+    printf("%d\n", y);
+    printf("%d\n", l);
+    printf("%f\n", x);
+    printf("%d\n", w);
+    printf("%ld\n", o);
+    printf("%f\n", e);
+    return 0;
+}
+
+
+
+
